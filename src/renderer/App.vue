3001e544c1158c87aaf1b51cb35d7d594c6e32af
Jorge Junior <jorgedjr21@gmail.com>
2020-03-22 03:23:08 -0300

    adds electron-vue

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'deezer-unofficial'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
